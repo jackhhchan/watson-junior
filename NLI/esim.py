f566cce66f812cee642305201487ed0a91b48ba4
Yunxue Hu <hereisloretta@gmail.com>
2019-05-18 21:06:44 +1000

    add NLI folder, abcnn + lstm + esim

---
+++ NLI/esim.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 17 10:50:19 2019
+
+@author: loretta
+"""
+
+from __future__ import print_function
+from keras import backend as K
+from keras.models import Model
+import numpy as np
+import keras
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
+import tensorflow as tf
+from gensim.models import Word2Vec
+from keras.callbacks import EarlyStopping,ModelCheckpoint
+import time
+import os
+from keras.models import load_model
+import tqdm
+import json
+from keras.layers import LSTM, Bidirectional, GlobalAveragePooling1D, Input,\
+      concatenate, Lambda, subtract, multiply, Dense, TimeDistributed, Embedding
+from NLI.attention import DotProductAttention
+from NLI.abcnn import word_embed_meta_data, create_train_dev_set
+from sentence_selection.generateTrainingFile import getPage_index,readOneFile      
+
+
+def plot(*args, **kwargs):
+
+    from keras.utils import plot_model as plt
+    plt(*args, **kwargs)
+
+
+
+def buildESIM(tokenizer,sentences_pair,sim,embed_dimensions,embedding_matrix,\
+                  max_len,num_classes,epoch,batch_size):
+    input_premise = Input(shape=(max_len,))
+    input_hypothesis = Input(shape=(max_len,))
+    inputs = [input_premise, input_hypothesis]
+
+#    embedding = ELMoEmbedding(output_mode=elmo_output_mode, idx2word=self.config.idx2token, mask_zero=mask_zero,
+#                              hub_url=elmo_model_url, elmo_trainable=elmo_trainable)
+#    premise_embed = embedding(input_premise)
+#    hypothesis_embed = embedding(input_hypothesis)
+    
+    train_data_x1, train_data_x2, train_labels, \
+    val_data_x1, val_data_x2, val_labels  = create_train_dev_set(
+            tokenizer, sentences_pair, sim, max_len, validation_split_ratio=0.1)
+    nb_words = len(tokenizer.word_index) + 1
+    emb_layer = Embedding(nb_words, embed_dimensions,input_length=max_len,
+                          weights=[embedding_matrix],
+                          trainable=False)
+    
+    premise_embed = emb_layer(input_premise)
+    hypothesis_embed = emb_layer(input_hypothesis)
+
+    bilstm_1 = Bidirectional(LSTM(units=300, return_sequences=True))
+    premise_hidden = bilstm_1(premise_embed)
+    hypothesis_hidden = bilstm_1(hypothesis_embed)
+
+    # local inference collected over sequences
+    premise_attend, hypothesis_attend = DotProductAttention()([premise_hidden, hypothesis_hidden])
+
+    # enhancement of local inference information
+    premise_enhance = concatenate([premise_hidden, premise_attend, subtract([premise_hidden, premise_attend]),
+                                   multiply([premise_hidden, premise_attend])])
+    hypothesis_enhance = concatenate([hypothesis_hidden, hypothesis_attend,
+                                      subtract([hypothesis_hidden, hypothesis_attend]),
+                                      multiply([hypothesis_hidden, hypothesis_attend])])
+
+    # inference composition
+    feed_forward = TimeDistributed(Dense(units=300, activation='relu'))
+    bilstm_2 = Bidirectional(LSTM(units=300, return_sequences=True))
+    premise_compose = bilstm_2(feed_forward(premise_enhance))
+    hypothesis_compose = bilstm_2(feed_forward(hypothesis_enhance))
+
+    global_max_pooling = Lambda(lambda x: K.max(x, axis=1))  # GlobalMaxPooling1D didn't support masking
+    premise_avg = GlobalAveragePooling1D()(premise_compose)
+    premise_max = global_max_pooling(premise_compose)
+    hypothesis_avg = GlobalAveragePooling1D()(hypothesis_compose)
+    hypothesis_max = global_max_pooling(hypothesis_compose)
+
+    
+    inference_compose = concatenate([premise_avg, premise_max, hypothesis_avg, hypothesis_max])
+
+    dense = Dense(units=300, activation='tanh')(inference_compose)
+    output = Dense(num_classes, activation='softmax')(dense)
+
+    model = Model(inputs, output)
+    model.compile(loss='categorical_crossentropy', metrics=['acc'], optimizer='rmsprop')
+    
+    early_stopping = EarlyStopping(monitor='val_loss', patience=5)
+
+    checkpoint_dir = './trained_model/ESIM_' + str(int(time.time()))
+
+    if not os.path.exists(checkpoint_dir):
+        os.makedirs(checkpoint_dir)
+
+    bst_model_path = checkpoint_dir + '.h5'
+
+    model_checkpoint = ModelCheckpoint(bst_model_path, monitor='val_acc', mode='auto',\
+                                       save_best_only=True, save_weights_only=False)
+
+    his = model.fit([train_data_x1, train_data_x2], train_labels,
+              validation_data=([val_data_x1, val_data_x2], val_labels),
+              epochs=epoch, batch_size=batch_size, shuffle=False,verbose=1,
+              callbacks=[early_stopping,model_checkpoint]
+                )
+    return model,his
+
+if __name__ == '__main__':
+    with open('resource/train.json') as data:
+            json_data = json.load(data)
+        
+    # read the json_file
+        
+    page_index, page_size = getPage_index()
+    
+    claims = []
+    evidences = []
+    labels = []
+    for j,item in enumerate(json_data):
+        if j < 100:
+        # only get first 100, [FOR NOW]
+            evds = []
+            if json_data[item]['evidence'] == []:
+                continue
+            for k,evd in enumerate(json_data[item]['evidence']):
+                if evd[0] in page_index:
+                    evds.append(readOneFile(page_index[evd[0]],evd[0],evd[1]))
+            claims.append(json_data[item]['claim'])
+            evidences.append(' '.join(evds))
+            labels.append(json_data[item]['label'])
+    
+    label_transfer = {'SUPPORTS':0,'REFUTES':1}
+    label_v2 = []
+    for l in labels:
+        if l in label_transfer:
+            label_v2.append(label_transfer[l])
+                
+    max_len = 64     # max length of one sentence
+    num_samples = len(claims)
+    num_classes = 2 # supports, refutes
+    embed_dimensions = 300
+    epoch = 50
+    batch_size = 1024
+
+    tokenizer, embedding_matrix = word_embed_meta_data(claims+evidences,mode = "Glove")
+    # return the embedding_matrix for sentences.
+    sentences_pair = [(x1, x2) for x1, x2 in zip(claims, evidences)]
+    sim = keras.utils.to_categorical(label_v2, num_classes)
+    
+    model,his = buildESIM(tokenizer,sentences_pair,sim,embed_dimensions,embedding_matrix,\
+                          max_len,num_classes,epoch,batch_size)
+    
+    
+#    plot(model, to_file='esim.png')
---
+++ NLI/normalLSTM.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 18 18:33:13 2019
+
+@author: loretta
+"""
+from keras.layers import Dense, Input, LSTM, Dropout, Bidirectional
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.layers.normalization import BatchNormalization
+from keras.layers.embeddings import Embedding
+from keras.layers.merge import concatenate
+from keras.callbacks import TensorBoard
+from keras.models import load_model
+from keras.models import Model
+from gensim.models.keyedvectors import KeyedVectors
+from keras import regularizers
+# std imports
+import time
+import gc
+import os
+from keras.preprocessing.sequence import pad_sequences
+import numpy as np
+
+
+def create_train_dev_set(tokenizer, sentences_pair, sim, max_len, validation_split_ratio=0.1):
+    sentences1 = [x[0] for x in sentences_pair]
+    sentences2 = [x[1] for x in sentences_pair]
+    train_sequences_1 = tokenizer.texts_to_sequences(sentences1)
+    train_sequences_2 = tokenizer.texts_to_sequences(sentences2)
+    leaks = [[len(set(x1)), len(set(x2)), len(set(x1).intersection(x2))]
+             for x1, x2 in zip(train_sequences_1, train_sequences_2)]
+
+    train_padded_data_1 = pad_sequences(train_sequences_1, maxlen=max_len)
+    train_padded_data_2 = pad_sequences(train_sequences_2, maxlen=max_len)
+    train_labels = np.array(sim)
+    leaks = np.array(leaks)
+
+    shuffle_indices = np.random.permutation(np.arange(len(train_labels)))
+    train_data_1_shuffled = train_padded_data_1[shuffle_indices]
+    train_data_2_shuffled = train_padded_data_2[shuffle_indices]
+    train_labels_shuffled = train_labels[shuffle_indices]
+    leaks_shuffled = leaks[shuffle_indices]
+
+    dev_idx = max(1, int(len(train_labels_shuffled) * validation_split_ratio))
+
+    del train_padded_data_1
+    del train_padded_data_2
+    gc.collect()
+
+    train_data_1, val_data_1 = train_data_1_shuffled[:-dev_idx], train_data_1_shuffled[-dev_idx:]
+    train_data_2, val_data_2 = train_data_2_shuffled[:-dev_idx], train_data_2_shuffled[-dev_idx:]
+    labels_train, labels_val = train_labels_shuffled[:-dev_idx], train_labels_shuffled[-dev_idx:]
+    leaks_train, leaks_val = leaks_shuffled[:-dev_idx], leaks_shuffled[-dev_idx:]
+
+    return train_data_1, train_data_2, labels_train, leaks_train, val_data_1, val_data_2, labels_val, leaks_val
+
+def create_test_data(tokenizer, test_sentences_pair, max_sequence_length):
+    """
+    Create training and validation dataset
+    Args:
+        tokenizer (keras.preprocessing.text.Tokenizer): keras tokenizer object
+        test_sentences_pair (list): list of tuple of sentences pairs
+        max_sequence_length (int): max sequence length of sentences to apply padding
+
+    Returns:
+        test_data_1 (list): list of input features for training set from sentences1
+        test_data_2 (list): list of input features for training set from sentences2
+    """
+    test_sentences1 = [x[0] for x in test_sentences_pair]
+    test_sentences2 = [x[1] for x in test_sentences_pair]
+
+    test_sequences_1 = tokenizer.texts_to_sequences(test_sentences1)
+    test_sequences_2 = tokenizer.texts_to_sequences(test_sentences2)
+    leaks_test = [[len(set(x1)), len(set(x2)), len(set(x1).intersection(x2))]
+                  for x1, x2 in zip(test_sequences_1, test_sequences_2)]
+
+    leaks_test = np.array(leaks_test)
+    test_data_1 = pad_sequences(test_sequences_1, maxlen=max_sequence_length)
+    test_data_2 = pad_sequences(test_sequences_2, maxlen=max_sequence_length)
+
+    return test_data_1, test_data_2, leaks_test
+
+
+
+def buildLSTM(tokenizer,sentences_pair,sim,embed_dimensions,embedding_matrix,\
+                number_lstm_units,rate_drop_lstm, rate_drop_dense, number_dense_units,\
+                max_len,num_classes,epoch,batch_size):
+    
+    train_data_x1, train_data_x2, train_labels, leaks_train,\
+    val_data_x1, val_data_x2, val_labels, leaks_val  = create_train_dev_set(
+            tokenizer, sentences_pair, sim, max_len, validation_split_ratio=0.1)
+    if train_data_x1 is None:
+        print("++++ !! Failure: Unable to train model ++++")
+        return None
+
+    nb_words = len(tokenizer.word_index) + 1
+
+    # Creating word embedding layer
+    embedding_layer = Embedding(nb_words, embed_dimensions, 
+                                    weights=[embedding_matrix],
+                                input_length=max_len, trainable=False)
+
+    # Creating LSTM Encoder
+    lstm_layer = Bidirectional(LSTM(number_lstm_units, dropout=rate_drop_lstm, recurrent_dropout=rate_drop_lstm))
+
+    # Creating LSTM Encoder layer for First Sentence
+    sequence_1_input = Input(shape=(max_len,), dtype='int32')
+    embedded_sequences_1 = embedding_layer(sequence_1_input)
+    x1 = lstm_layer(embedded_sequences_1)
+
+    # Creating LSTM Encoder layer for Second Sentence
+    sequence_2_input = Input(shape=(max_len,), dtype='int32')
+    embedded_sequences_2 = embedding_layer(sequence_2_input)
+    x2 = lstm_layer(embedded_sequences_2)
+
+    # Creating leaks input
+    leaks_input = Input(shape=(3,))
+    leaks_dense = Dense(int(number_dense_units/2), activation='relu')(leaks_input)
+
+    # Merging two LSTM encodes vectors from sentences to
+    # pass it to dense layer applying dropout and batch normalisation
+    merged = concatenate([x1, x2, leaks_dense])
+    merged = BatchNormalization()(merged)
+    merged = Dropout(rate_drop_dense)(merged)
+    merged = Dense(number_dense_units, activation='relu')(merged)
+    merged = BatchNormalization()(merged)
+    merged = Dropout(rate_drop_dense)(merged)
+#        preds = Dense(num_classes, activation='sigmoid')(merged)
+#        merged = Dense(self.number_dense_units, activation=self.activation_function,kernel_regularizer=regularizers.l2(0.0001))(merged)
+#        merged = BatchNormalization()(merged)
+#        merged = Dropout(self.rate_drop_dense)(merged)
+
+    preds = Dense(output_dim=num_classes, activation='softmax')(merged)
+#        model.summary()
+
+    
+    model = Model(inputs=[sequence_1_input, sequence_2_input, leaks_input], outputs=preds)
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['acc'])
+
+    early_stopping = EarlyStopping(monitor='val_loss', patience=5)
+
+#    STAMP = 'lstm_%d_%d_%.2f_%.2f' % (self.number_lstm_units, self.number_dense_units, self.rate_drop_lstm, self.rate_drop_dense)
+
+    checkpoint_dir = './trained_model/LSTM_' + str(int(time.time())) + '/'
+
+    if not os.path.exists(checkpoint_dir):
+        os.makedirs(checkpoint_dir)
+
+    bst_model_path = checkpoint_dir + '.h5'
+
+    model_checkpoint = ModelCheckpoint(bst_model_path, save_best_only=True, save_weights_only=False)
+
+#    tensorboard = TensorBoard(log_dir=checkpoint_dir + "logs/{}".format(time.time()))
+
+    his = model.fit([train_data_x1, train_data_x2, leaks_train], train_labels,
+              validation_data=([val_data_x1, val_data_x2, leaks_val], val_labels),
+              epochs=epoch, batch_size=batch_size, shuffle=False,verbose=1,
+              callbacks=[early_stopping,model_checkpoint])
+    
+    
+    return model,his
